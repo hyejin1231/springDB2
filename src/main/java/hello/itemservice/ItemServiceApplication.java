99e034bd06465e828ef7f7b69796091c26ca7c83
hyejin <phj981231@naver.com>
2022-07-06 15:43:39 +0900

    20220706 #2. 데이터 접근 기술 - 스프링 JdbcTemplate SimpleJdbcInsert 사용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.Profile;
 
 //@Import(MemoryConfig.class) // MemoryConfig 파일을 설정 파일로 사용
 //@Import(JdbcTemplateV1Config.class)
-@Import(JdbcTemplateV2Config.class)
+//@Import(JdbcTemplateV2Config.class)
+@Import({JdbcTemplateV3Config.class})
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web") // 컨트롤러만 컴포넌트 스캔하도록
 public class ItemServiceApplication {
 
