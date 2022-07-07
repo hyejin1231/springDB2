4d98a11b0d9dfe54675f65c96719e326b3e80d1d
hyejin1231 <phj981231@naver.com>
2022-07-07 10:30:58 +0900

    20220706 #1. 데이터 접근 기술 - 테스트 테스트 :  스프링 부트와 임베디드 모드

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,15 +2,20 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 //@Import(MemoryConfig.class) // MemoryConfig 파일을 설정 파일로 사용
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
+@Slf4j
 @Import({JdbcTemplateV3Config.class})
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web") // 컨트롤러만 컴포넌트 스캔하도록
 public class ItemServiceApplication {
@@ -25,5 +30,22 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+	/**
+	 * Profile("test")에서 임베디드 모드로 데이터베이스를 사용하기 위해서 설정
+	 * @return
+	 */
+	/*
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"); // 메모리 모드로 동작하는 h2 데이터베이스 사용
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+	*/
 }
 
