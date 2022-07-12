bd6ec76f727d756ece0f5b1d9cb024ce9ed10253
hyejin1231 <phj981231@naver.com>
2022-07-12 16:36:39 +0900

    20220712 데이터 접근 기술 - JPA JPA 적용 - 개발

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -17,7 +17,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV2Config.class)
 @Slf4j
 //@Import({JdbcTemplateV3Config.class})
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web") // 컨트롤러만 컴포넌트 스캔하도록
 public class ItemServiceApplication {
 
--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,18 +2,25 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 /**
  * 상품 객체를 나타내는 Item
  */
 @Data
+@Entity // JPA가 사용하는 객체라는 뜻
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에서 값을 넣어주는 옵션
     private Long id; // 기본 키
 
+    @Column(name = "item_name", length = 10)
     private String itemName; // 상품명
+
     private Integer price; // 상품 가격
     private Integer quantity; // 수량
 
+    // JPA는 public 또는 protected 기본 생성자가 필수로 있어야 한다. -> 프록시 기술 같은 것을 사용하기 위해서...
     public Item() {
     }
 
